6cfee98daa9c1ab8b22b362907c8e1248218a302
Lenoch <lenoch@mail.local>
2017-09-23 22:43:49 +0200

    First hello

---
+++ hello-app/src/main/java/cz/muni/fi/PA165/app/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.PA165.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
